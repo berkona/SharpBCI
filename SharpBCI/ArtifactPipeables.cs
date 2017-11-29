6bfd5b30e2977c740217732d53ab9b33593959d4
Jon Monroe <berkona@gmail.com>
2017-11-28 21:21:48 -0500

    Docs docs docs

--- SharpBCI/ArtifactPipeables.cs
+++ SharpBCI/ArtifactPipeables.cs
@@ -2,16 +2,31 @@
 using System.Linq;
 
 namespace SharpBCI {
-	
+
+	/**
+	 * An artifact detector which uses a TournamentArtifactDectector on a per-channel basis to detect artifacts
+	 * Input types: EEG, Output types: EEG
+	 */
 	public class TournamentArtifactPipeable : Pipeable {
 
-		IArtifactDetector[] detectors;
+		readonly IArtifactDetector[] detectors;
 
+		/**
+		 * Create a new TournamentArtifactPipeable with following params
+		 * 
+		 * @param channels channels as reported by the underlying EEGDeviceAdapter
+		 * @param sampleRate sample rate as reported by the underlying EEGDeviceAdapter
+		 * @param learningTime This many seconds of data will be used to fit the underlying TournamentArtifactPipeable
+		 * @param tournamentSize how many IArtifactDetectors should compete
+		 * @param nAccept the majority of the top nAccept IArtifactDetector's determines if the EEGEvent is considered to be an artifact
+		 * @param initialMerits determines how quickly an IArtifactDetector is thrown out of the tournament in the worst case.  Should be large in relation to sampleRate.
+		 * 
+		 */
 		public TournamentArtifactPipeable(int channels, double sampleRate, double learningTime, uint tournamentSize, uint nAccept, uint initialMerits) {
 			uint learningSampleSize = (uint) Math.Round(learningTime * sampleRate);
 			detectors = new IArtifactDetector[channels];
 			for (int i = 0; i < channels; i++) {
-				detectors[i] = new TournamentArtifactDectector(tournamentSize, learningSampleSize, nAccept, initialMerits);
+				detectors[i] = new TournamentArtifactDetector(tournamentSize, learningSampleSize, nAccept, initialMerits);
 			}
 		}
 
